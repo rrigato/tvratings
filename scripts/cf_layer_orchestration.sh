ebc175484567d8be5961118053a1b53bdf0f15c4
rrigato <ryanrigato@gmail.com>
2022-05-22 12:05:48 -0500

    ask-sdk-core-most-recent lambda layer manually tested

---
+++ scripts/cf_layer_orchestration.sh
@@ -0,0 +1,13 @@
+aws s3api put-object --bucket lambda-deployment-bundles \
+    --key layers/ask_sdk_core_most_recent.zip \
+    --body layers/ask_sdk_core_1_17_1.zip
+
+
+aws cloudformation create-change-set \
+    --stack-name alexa-skills-kit-sdk-python-layer \
+    --change-set-name tvratings-new-layer-version \
+    --template-body file://templates/ask_lambda_layer.template 
+
+aws cloudformation execute-change-set \
+    --stack-name alexa-skills-kit-sdk-python-layer \
+    --change-set-name tvratings-new-layer-version 
--- scripts/repo_owned_stacks.sh
+++ scripts/repo_owned_stacks.sh
@@ -1,3 +1,4 @@
 aws cloudformation create-stack --stack-name tvratings-alexa-skill \
 --template-body file://templates/tvratings_alexa_skill.template \
 --tags Key=project,Value=tvratings Key=prod,Value=yes Key=multi_repo,Value=yes
+
