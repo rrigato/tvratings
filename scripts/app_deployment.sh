4efdbfbb547c23993087cadf41dc25cf57fd59e7
rrigato <ryanrigato@gmail.com>
2022-06-03 18:42:11 -0500

    Update lambda function logic

--- scripts/app_deployment.sh
+++ scripts/app_deployment.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-export BUNDLE_DIR_NAME="deployment"
-export PROJECT_NAME="tvratings"
-export DEPLOYMENT_PACKAGE="${PROJECT_NAME}_deployment_package.zip"
-
 #exits program immediately if a command is not sucessful
 set -e
 
---
+++ scripts/orchestrate_deployment.sh
@@ -0,0 +1,6 @@
+
+export BUNDLE_DIR_NAME="deployment"
+export PROJECT_NAME="tvratings"
+export DEPLOYMENT_PACKAGE="${PROJECT_NAME}_deployment_package.zip"
+
+scripts/app_deployment.sh
