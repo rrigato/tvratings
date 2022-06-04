56b9d513e4ad8ed9a0577a9f850550f4eadea01e
rrigato <ryanrigato@gmail.com>
2022-06-04 13:10:29 -0500

    parameterized the test case

--- tests/test_repo/test_tvratings_backend.py
+++ tests/test_repo/test_tvratings_backend.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from unittest.mock import MagicMock
 import unittest
 
+
 class TestTvratingsBackend(unittest.TestCase):
 
     @patch("boto3.resource")
@@ -10,19 +11,30 @@ class TestTvratingsBackend(unittest.TestCase):
         """Happy Path dynamodb call succeeds"""
         from datetime import date
         from fixtures.ratings_fixtures import fake_dynamodb_query_response
+        from tvratings.entities.entity_model import TelevisionRating
         from tvratings.repo.tvratings_backend import load_one_date
         
         mock_rating_night = date(2014,1,4)
-        mock_num_time_slots = 0
+        mock_num_time_slots = [0, 3, 5]
 
-        boto3_resource_mock.return_value.Table.return_value = fake_dynamodb_query_response(
-            mock_num_time_slots
-        )
+        for mock_num_time_slot in mock_num_time_slots:
+            with self.subTest(mock_num_time_slot=mock_num_time_slot):
 
-        television_ratings_entities, ratings_retrieval_error = load_one_date(mock_rating_night)
+                boto3_resource_mock.return_value.Table.return_value = fake_dynamodb_query_response(
+                    mock_num_time_slot
+                )
 
-        self.assertIsInstance(television_ratings_entities, list)
-        self.assertIsNone(ratings_retrieval_error)
+                television_ratings_entities, ratings_retrieval_error = load_one_date(mock_rating_night)
+
+                for tv_show in television_ratings_entities:
+                    self.assertIsInstance(tv_show, TelevisionRating)
+
+                self.assertEqual(len(television_ratings_entities), 
+                    mock_num_time_slot,
+                    msg=( "\n\n load_one_date is not returning the same number of " + 
+                    "TelevisionRating entities as Items returns from DynamoDB")
+                )
+                self.assertIsNone(ratings_retrieval_error)
 
     
 
