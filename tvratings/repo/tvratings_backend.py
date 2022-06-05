d7d57593a3e01baf56a5dc4c7901bc526b9fd97c
rrigato <ryanrigato@gmail.com>
2022-06-05 08:10:08 -0500

    fixture for TelevisionRating attribute name

--- tvratings/repo/tvratings_backend.py
+++ tvratings/repo/tvratings_backend.py
@@ -1,6 +1,5 @@
 from boto3.dynamodb.conditions import Key
 from datetime import date
-from fixtures.ratings_fixtures import get_mock_television_ratings
 from typing import Union
 from tvratings.entities.entity_model import TelevisionRating
 
@@ -9,10 +8,12 @@ import logging
 import os
 
 
-def _convert_dynamodb_query_to_entity(dynamodb_items: list) -> None:
+def _convert_dynamodb_query_to_entity(dynamodb_items: list) -> list[TelevisionRating]:
     """Converts DynamoDB external query to list of TelevisionRating entities
     """
     logging.info("_convert_dynamodb_query_to_entity - invocation begin")
+
+    all_tvratings = []
 
     for dynamodb_item in dynamodb_items:
         tv_show = TelevisionRating()
@@ -25,9 +26,11 @@ def _convert_dynamodb_query_to_entity(dynamodb_items: list) -> None:
         tv_show.rating = dynamodb_item.get("TOTAL_VIEWERS")
         tv_show.rating_18_49 = dynamodb_item.get("TOTAL_VIEWERS_AGE_18_49")
 
-    logging.info("_convert_dynamodb_query_to_entity - invocation end")
+        all_tvratings.append(tv_show)
 
-    return(None)
+    logging.info(f"_convert_dynamodb_query_to_entity - len(all_tvratings) - {len(all_tvratings)}")
+
+    return(all_tvratings)
 
 
 def load_one_date(ratings_occurred_on: date) -> tuple[Union[list[TelevisionRating], None], 
@@ -36,25 +39,38 @@ def load_one_date(ratings_occurred_on: date) -> tuple[Union[list[TelevisionRatin
 
         Parameters
         ----------
-        ratings_occurred_on: datetime.date
+        ratings_occurred_on
             Saturday night ratings date
 
         Returns
         -------
-        television_ratings: list
+        television_ratings
             Each element is a TelevisionRating entity
             No matching ratings for ratings_occurred_on returns []
             Any unexpected processing errors returns None
 
-        unexpected_error: None
+        unexpected_error
             str if unable to load the television ratings from persistent storage 
     """
+
     dynamodb_table = boto3.resource("dynamodb", os.environ.get("AWS_REGION")).Table(
         "prod_toonami_ratings"
     )
 
+    logging.info("load_one_date - obtained table resource")
 
-    return(dynamodb_table.query(
+    dynamodb_response = dynamodb_table.query(
         KeyConditionExpression=Key("RATINGS_OCCURRED_ON").eq(ratings_occurred_on.isoformat())
-    ), None)
+    )
+
+    logging.info("load_one_date - obtained dynamodb_response")
+
+    if len(dynamodb_response["Items"]) == 0:
+        logging.info("load_one_date - dynamodb_response Items list has no elements")
+        return([], None)
+
+
+    logging.info("load_one_date - invoking _convert_dynamodb_query_to_entity")
+
+    return(_convert_dynamodb_query_to_entity(dynamodb_response["Items"]), None)
     
