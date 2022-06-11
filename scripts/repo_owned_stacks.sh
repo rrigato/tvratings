b61d0ff95ab2a57ce7d6b079060f9c3521ee4ac9
rrigato <ryanrigato@gmail.com>
2022-06-11 18:13:43 -0500

    command for alexa skill

--- scripts/repo_owned_stacks.sh
+++ scripts/repo_owned_stacks.sh
@@ -6,3 +6,8 @@ aws cloudformation update-stack --stack-name tvratings-alexa-skill \
 --template-body file://templates/tvratings_alexa_skill.template \
 --tags Key=project,Value=tvratings Key=prod,Value=yes Key=multi_repo,Value=yes
 
+
+aws cloudformation create-stack --stack-name tvratings-alexa-skill-permissions \
+--template-body file://templates/alexa_skill_permissions.template \
+--parameters ParameterKey=alexaSkillId,ParameterValue=<add_alexa_skill_id> \
+--tags Key=project,Value=tvratings Key=prod,Value=yes Key=multi_repo,Value=yes
