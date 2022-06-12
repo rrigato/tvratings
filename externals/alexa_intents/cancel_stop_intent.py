be8c29d4acea83b32a1c8b7730986287515fd16f
rrigato <ryanrigato@gmail.com>
2022-06-12 10:45:13 -0500

    Type hinting for Cancel/stop intent

--- externals/alexa_intents/cancel_stop_intent.py
+++ externals/alexa_intents/cancel_stop_intent.py
@@ -1,17 +1,15 @@
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_core.utils import is_intent_name
+from ask_sdk_model.response import Response
 
 import logging
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """User requests AMAZON.CancelIntent or AMAZON.StopIntent during a session"""
 
-    def can_handle(self, handler_input):
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         """Determines the type of input the class can handle
-
-            Parameters
-            ----------
-            handler_input: ask_sdk_core.handler_input.HandlerInput
 
             Returns
             -------
@@ -21,17 +19,8 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
         return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
                 is_intent_name("AMAZON.StopIntent")(handler_input))
 
-    def handle(self, handler_input):
-        """Applies business logic for the appropriate class handler
-
-            Parameters
-            ----------
-            handler_input: ask_sdk_core.handler_input.HandlerInput
-
-            Returns
-            -------
-            alexa_sdk_response: ask_sdk_model.Response
-        """
+    def handle(self, handler_input: HandlerInput) -> Response:
+        """Applies business logic for the appropriate class handler"""
         
         speak_output = """
             <prosody rate="x-slow">
