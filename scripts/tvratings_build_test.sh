a70284f48b8b601f4d0ebf09c9775c5e915b60df
rrigato <ryanrigato@gmail.com>
2024-05-26 17:08:54 -0500

    unit tests in vm

--- scripts/tvratings_build_test.sh
+++ scripts/tvratings_build_test.sh
@@ -1,2 +1,27 @@
-echo "to do"
-echo $AWS_ACCOUNT_ID
+set -e
+
+
+export BUCKET_NAME="${PROJECT_NAME}-app-artifacts"
+export DEPLOYMENT_PACKAGE="${PROJECT_NAME}_deployment_package.zip"
+
+
+python -m venv avenv
+
+source avenv/bin/activate
+pip install -r requirements/requirements-dev.txt
+
+secret_scan_results=$(detect-secrets scan | \
+python -c "import sys, json; print(json.load(sys.stdin)['results'])" )
+
+# static scan for security credentials that terminates if any secrets are found
+if [ "${secret_scan_results}" != "{}" ]; then
+    echo "detect-secrets scan failed"
+    exit 125
+fi
+
+
+python -m unittest
+
+deactivate
+
+echo "--------beginning bundle--------"
